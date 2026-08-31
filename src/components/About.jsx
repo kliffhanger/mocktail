d78eb24e1c9fe2acb1cf2dc1f3b0a2beeea4db0a
kliffhanger <30635176+kliffhanger@users.noreply.github.com>
2026-08-31 14:52:00 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -65,7 +65,7 @@ const About = () => {
                 <div className='top-grid'>
                     <div className='md:col-span-3'>
                         <div className='noisy'>
-                            <img src='/public/images/abt1.png' alt='grid-img-1' />
+                            <img src='/images/abt1.png' alt='grid-img-1' />
                         </div>
                     </div>
                     <div className='md:col-span-6'>
